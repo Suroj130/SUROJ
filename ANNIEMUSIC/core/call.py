66307e77bc473a4b84dd953f3f5f8672138e70c7
Suroj130 <sandipbera.ind@gmail.com>
2026-03-19 22:05:15 +0530

    Update call.py

--- ANNIEMUSIC/core/call.py
+++ ANNIEMUSIC/core/call.py
@@ -4,10 +4,12 @@ from datetime import datetime, timedelta
 from typing import Union
 
 from pyrogram import Client
+from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
 from pytgcalls.types import StreamAudioEnded, Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+# ... বাকি কোড
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
